7986d8a0182a2d4fa2a6ae4f4be96ea888786361
Dharmu Kumar <dharmukumar1622003@gmail.com>
2026-04-22 11:46:30 +0530

    Update index.js

--- JavaScript/Day_9_Date/index.js
+++ JavaScript/Day_9_Date/index.js
@@ -1,6 +1,11 @@
-const d = new Date();
+const d = new Date(); // millesecond formate to claclut time,date,day    calclute a date or time 1 jan 1970
 // console.log(d.toISOString())
 // console.log(d.toString())
 // console.log(d.toDateString())
 
-console.log(d)
+// console.log(d.getDate())
+// console.log(d.getTime())
+// console.log(d.getMonth())
+
+const now = Date.now()  // directly a calclute a millesecond
+console.log(now)  
