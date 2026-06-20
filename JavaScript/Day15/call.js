537442d71e87bef0cefba9a888e68959f54a4354
Dharmu Kumar <dharmukumar1622003@gmail.com>
2026-06-21 00:35:28 +0530

    Create call.js

---
+++ JavaScript/Day15/call.js
@@ -0,0 +1,16 @@
+//call back function
+
+function name(fun){
+    consle.log("Dharmu");
+    fun();  // This will call the function passed as an argument)
+}
+
+function greet()
+{
+    console.log("I am a call back function");
+}
+
+// name();   //
+// greet();
+
+name(greet);  //this is a call back function because we are passing a function as an argument to another function
