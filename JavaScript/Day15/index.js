8246062e0c6e6878b068bb194565d701aa24d7b7
Dharmu Kumar <dharmukumar1622003@gmail.com>
2026-06-22 21:46:43 +0530

    Create index.js

---
+++ JavaScript/Day15/index.js
@@ -0,0 +1,50 @@
+// let user = {
+//     name : " Dharmu",
+//     age: 30
+// }
+// //console.log(Object.getOwnPropertyDescriptor(user, "name")) 
+
+// Object.defineProperty(user, "name", {
+//     writable: false,
+ 
+// });
+
+// user.name = "Dharmendra"  // This will not change the name property because it is not writable
+
+
+
+// for  of loop
+
+// const arr = [10,20,30,11,18,13];
+
+// for(let value of arr)
+//     {
+//         console.log(value);
+//     }
+
+//     let str = "Dharmendra";   
+//     for (let char of str)
+//     {
+//         console.log(char);
+//     }
+
+// dont use for of loop in objects because it will not work
+
+// const obj = {
+//     name: "Dharmu",
+//     age: 30
+
+// }
+
+// for (let key in obj)
+//     {
+//         console.log(key);  // This will print the keys of the object
+//         console.log(obj[key]);  // This will print the values of the object
+//     }
+
+// for(let valuse of Object.keys(obj))
+// {
+//     console.log(valuse);  // This will print the keys of the object
+//     console.log(obj[valuse]);  // This will print the values of the object
+// }
+
